dff1706caa48ce0a97de8dd069a070a37bba113e
Albalad <alvaro.albalad@gmail.com>
2024-10-02 18:08:28 +0000

    Eliminado contenido hacerlo desde ramas aparte

--- src/app.js
+++ src/app.js
@@ -1,52 +1,11 @@
+/* eslint-disable */
+import "bootstrap";
 import "./style.css";
 
-// Arrays con palabras
-let Quien = [
-  "4Geeks ",
-  "Pikachu ",
-  "Gozilla ",
-  "Trotuman ",
-  "Terminator ",
-  "Rayo Mcqueen "
-];
-let Que = [
-  "atropelló ",
-  "desintegró ",
-  "electrocutó ",
-  "teletransportó ",
-  "abrazó "
-];
-let Objeto = [
-  "a un cactus ",
-  "a mi perro ",
-  "a mi pais ",
-  "mi portatil ",
-  "mi universo ",
-  "mi chancla ",
-  "mi router "
-];
-let CuandoComo = [
-  "mientras comía.",
-  "sin dejar rastro.",
-  "apasionadamente.",
-  "a base de bugs.",
-  "sin descanso",
-  "con mi ayuda"
-];
+import "./assets/img/rigo-baby.jpg";
+import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  function excusa() {
-    // Randomizar las palabras
-    let rdm1 = Math.floor(Math.random() * Quien.length);
-    let rdm2 = Math.floor(Math.random() * Que.length);
-    let rdm3 = Math.floor(Math.random() * Objeto.length);
-    let rdm4 = Math.floor(Math.random() * CuandoComo.length);
-
-    //Creamos el resultado
-    let resultado = Quien[rdm1] + Que[rdm2] + Objeto[rdm3] + CuandoComo[rdm4];
-    return resultado;
-  }
-
-  // Creamos la excusa y la ponemos en el h1 con id excusa
-  document.querySelector("#excusa").innerHTML = excusa();
+  //write your code here
+  console.log("Hello Rigo from the console!");
 };
